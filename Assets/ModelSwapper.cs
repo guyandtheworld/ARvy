4b6e5585583e6180d8f3bb19abe63c60e386c09a
Adarsh S <4darshofficial@gmail.com>
2018-03-15 23:58:18 +0530

    First Commit

---
+++ Assets/ModelSwapper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+public class ModelSwapper : MonoBehaviour {
+
+    public AnchorStageBehaviour theTrackable;
+
+    //Models management.
+    private Transform scene;
+
+    // For swapping models.
+    public GameObject checkSwap;
+    Selecter skeletor;
+
+    // Use this for initialization
+    void Start()
+    {
+        skeletor = checkSwap.GetComponent<Selecter>();
+        if (theTrackable == null)
+        {
+            Debug.Log("Warning: Trackable not set !!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+
+    }
+
+    public void SwapModel(string ModelName)
+    {
+        //Loading sphere and cube from resource folder.
+        // You have to convert this to using AssetBundles.
+        scene = (Transform)Resources.Load("prefabs/" + ModelName, typeof(Transform));
+
+        GameObject trackableGameObject = theTrackable.gameObject;
+
+        //disable any pre-existing augmentation
+        for (int i = 0; i < trackableGameObject.transform.childCount; i++)
+        {
+            Transform child = trackableGameObject.transform.GetChild(i);
+            child.gameObject.SetActive(false);
+        }
+
+        Transform model = GameObject.Instantiate(scene) as Transform;
+        // Re-parent the cube as child of the trackable gameObject
+        model.transform.parent = theTrackable.transform;
+        skeletor.resetSwapModel();
+    }
+}
---
+++ Assets/Selecter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This is used as a connection for connecting the audio to the model swapper.
+// The audio signals are received here and swap function is called.
+
+public class Selecter : MonoBehaviour {
+
+    private bool SwapModel = true;
+
+    // For receiving audio from Watson.
+    public GameObject audioWat;
+    AudioStreaming audioStreamer;
+
+    // For swapping models.
+    public GameObject swapper;
+    ModelSwapper modelSwapper;
+
+    //Testing
+    private System.Random rnd;
+    private List<string> iList = new List<string>();
+
+    public void resetSwapModel()
+    {
+        SwapModel = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        // Testing
+        rnd = new System.Random();
+        iList.Add("sphere");
+        iList.Add("cube");
+
+        audioStreamer = audioWat.GetComponent<AudioStreaming>();
+        modelSwapper = swapper.GetComponent<ModelSwapper>();
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 50, 120, 40), "Swap Model"))
+        {
+            if (SwapModel)
+                SwapModel = false;
+            else
+                SwapModel = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // Randomly takes an item in the iList and displays it.
+        if (SwapModel)
+            modelSwapper.SwapModel((string)iList[rnd.Next(iList.Count)]);
+    }
+}

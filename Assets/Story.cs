318c2dd0767c702f2accf3fcb0bf5b78a570da54
Adarsh S <4darshofficial@gmail.com>
2018-04-21 13:46:32 +0530

    Add a menu to the game

---
+++ Assets/Story.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story : MonoBehaviour {
+
+    public string storyTitle;
+    public Sprite sprite;
+
+}
---
+++ Assets/StoryDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryDisplay : MonoBehaviour {
+
+    public Text storyTitle;
+    public Image storyCover;
+
+    public Story story;
+
+	void Start () {
+        if (story != null) Initialize(story);
+	}
+	
+    public void Initialize(Story story)
+    {
+        this.story = story;
+        if (storyTitle != null)
+            storyTitle.text = story.storyTitle;
+        if (storyCover != null)
+            storyCover.sprite = story.sprite;
+    }
+}
